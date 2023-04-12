271004f3a480affd8f461ea1a1dc915effa4cd13
Mark Jensen <mark.jensen.238@gmail.com>
2023-04-12 16:38:22 -0400

    moved methods to Car class

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Car implements Comparable<Car> {
     private String make;
     private String model;
@@ -34,6 +42,49 @@ public class Car implements Comparable<Car> {
 
     public void setYear(String year){
         this.year = year;
+    }
+
+    public static void serializeAsCsv(Car car, String fileName){
+        String carAttributes = prePrintCsv(car);
+        System.out.println(carAttributes);
+
+        Path desktopPath = Paths.get(System.getProperty("user.home"), "Desktop");
+        Path filePath = desktopPath.resolve(fileName);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+            writer.write(carAttributes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String prePrintCsv(Car car){
+        return car.getYear()+","+car.getMake()+","+car.getModel();
+    }
+
+    public static Car deserializeFromCsv(String fileName){
+        Path desktopPath = Paths.get(System.getProperty("user.home"), "Desktop");
+        Path filePath = desktopPath.resolve(fileName);
+
+        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+            String carData = reader.readLine();
+            String[] carAttributes = carData.split(",");
+
+            if (carAttributes.length != 3) {
+                throw new IllegalArgumentException("Invalid data format");
+            }
+
+            String year = carAttributes[0].trim();
+            String make = carAttributes[1].trim();
+            String model = carAttributes[2].trim();
+
+            return new Car(year, make, model);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public int compareTo(Car other) {
